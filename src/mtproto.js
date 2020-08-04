101b4907ffb54cefd11268db2785e0d2ea058e3c
Lucas <lucas.ansei@appcivico.com>
2020-08-04 15:35:34 -0300

    Adding mtproto class

---
+++ src/mtproto.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const { MTProto } = require('@mtproto/core');
+
+const api = {
+  call(tempLocalStorage, method, params, options = {}) {
+
+    const mtproto = new MTProto({
+        api_id: process.env.API_ID,
+        api_hash: process.env.API_HASH,
+        customLocalStorage: tempLocalStorage,
+    });
+
+    return mtproto.call(method, params, options).catch(async error => {
+      console.log(`${method} error:`, error);
+
+      const { error_code, error_message } = error;
+
+      if (error_code === 303) {
+        const [type, dcId] = error_message.split('_MIGRATE_');
+
+        // If auth.sendCode call on incorrect DC need change default DC, because call auth.signIn on incorrect DC return PHONE_CODE_EXPIRED error
+        if (type === 'PHONE') {
+          await mtproto.setDefaultDc(+dcId);
+        } else {
+          options = {
+            ...options,
+            dcId: +dcId,
+          };
+        }
+
+        return this.call(method, params, options);
+      }
+
+      return Promise.reject(error);
+    });
+  },
+};
+
+module.exports = api;
