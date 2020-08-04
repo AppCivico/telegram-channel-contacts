b3efc12cd4bd5c600ae88a71d08c6e20bf4e76e8
Lucas <lucas.ansei@appcivico.com>
2020-08-04 15:35:25 -0300

    Adding index.js and main methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+require('dotenv').config();
+
 const restify = require('restify');
-const errors = require('restify-errors');
-const logger = require('morgan');
+const errors  = require('restify-errors');
+const logger  = require('morgan');
+const api     = require('./mtproto.js');
+const { MTProto } = require('@mtproto/core');
+
+const Storage = require('node-localstorage').LocalStorage;
+
+class TempLocalStorage {
+    constructor(team_robot_id) {
+        this.storage = new Storage('../storage/team_robot_id_' + team_robot_id + '-auth');
+    }
+
+    setItem(key, value) {
+      return this.storage.setItem(key, value);
+    }
+
+    getItem(key) {
+      return this.storage.getItem(key);
+    }
+}
+
 
 /**
   * Initialize server
@@ -8,8 +29,8 @@ const logger = require('morgan');
 const server = restify.createServer();
 
 /**
-  * Middlewares
-  */
+ * Middlewares
+ */
 // server.use(restify.plugins.jsonp());
 server.use(logger('dev'));
 server.use(restify.plugins.queryParser());
@@ -18,57 +39,135 @@ server.pre(restify.plugins.pre.dedupeSlashes());
 /**
   * Services
   */
-const getChannelContacts = async (q, args = {}) => {
-  let tsRank = '';
-  let tsQuery = '';
-  let whereCond = '';
-  let orderBy = 'ORDER BY artigos.created_at DESC';
+const sendCode = async (team_robot_id, phone) => {
+    let tempLocalStorage = new TempLocalStorage(team_robot_id);
 
+    let sendCodeReq = await api.call(tempLocalStorage, 'auth.sendCode', {
+        phone_number: phone,
+        settings: {
+            _: 'codeSettings',
+        },
+    });
 
-  console.log(sqlQuery);
-  console.dir({
-    q,
-    orderBy,
-    whereCond,
-    limit,
-    offset,
-    tsRank,
-  });
+    return sendCodeReq;
+};
 
-  // Retrieve results
-  const results = await db.any(sqlQuery, {
-    q,
-    orderBy,
-    whereCond,
-    limit,
-    offset,
-    tsRank,
-  });
+const signIn = async (team_robot_id, phone, code, phone_code_hash) => {
+    let tempLocalStorage = new TempLocalStorage(team_robot_id);
 
-  // Pagination flag
-  const hasMore = Boolean(results[limit]);
+    let signInReq = await api.call(tempLocalStorage, 'auth.signIn', {
+        phone_number:    phone,
+        phone_code:      code,
+        phone_code_hash: phone_code_hash,
+    });
 
-  return {
-  };
+    return signInReq;
+}
+
+const getChannels = async (team_robot_id) => {
+    let tempLocalStorage = new TempLocalStorage(team_robot_id);
+
+    let chats = await api.call(
+        tempLocalStorage,
+        'messages.getAllChats',
+        { except_ids: [] },
+    );
+
+    let channels = chats.chats.filter( chat => chat._ === 'channel' && chat.admin_rights );
+    channels.forEach( channel => {
+            delete channel.pFlags;
+            delete channel.version;
+            delete channel.default_banned_rights;
+            delete channel.photo;
+            delete channel.admin_rights;
+            delete channel.flags;
+            delete channel._;
+        }
+    );
+
+    return channels;
+}
+
+const getChannelContacts = async (team_robot_id, channel_id, access_hash, offset) => {
+
+    let tempLocalStorage = new TempLocalStorage(team_robot_id);
+
+    let channel_members_req = await api.call(
+        tempLocalStorage,
+        'channels.getParticipants',
+        {
+            offset: 0,
+            limit:  100,
+            channel: {
+                _ : 'inputChannel',
+                channel_id: channel_id,
+                access_hash: access_hash
+            },
+            filter: { _ : 'channelParticipantsRecent' }
+        },
+    );
+
+    let members = channel_members_req.users;
+
+    return members;
 };
 
 /**
   * Routes
   */
-server.post('/channel-contacts', async (req, res, next) => {
-  const { _q, _limit, _offset } = req.query;
+server.post('/send-code', async (req, res, next) => {
+    const { team_robot_id, phone } = req.query;
 
-  try {
-    const data = await getChannelContacts(_q, { limit: _limit, offset: _offset });
-    res.send(data);
-    return next();
-  } catch (err) {
-    console.log(err);
-    return next(new errors.InternalServerError('Internal server error'));
-  }
+    try {
+        const data = await sendCode(team_robot_id, phone);
+        res.send(data);
+        return next();
+    } catch (err) {
+        console.log(err);
+        return next(new errors.InternalServerError('Internal server error'));
+    }
+});
+
+server.post('/sign-in', async (req, res, next) => {
+    const { team_robot_id, phone, code, phone_code_hash  } = req.query;
+
+    try {
+        const data = await signIn(team_robot_id, phone, code, phone_code_hash);
+        res.send(data);
+        return next();
+    } catch (err) {
+        console.log(err);
+        return next(new errors.InternalServerError('Internal server error'));
+    }
+});
+
+server.get('/channels', async (req, res, next) => {
+    const { team_robot_id } = req.query;
+
+    try {
+        const data = await getChannels(team_robot_id);
+        res.send(data);
+        return next();
+    } catch (err) {
+        console.log(err);
+        return next(new errors.InternalServerError('Internal server error'));
+    }
+});
+
+server.get('/channel-contacts', async (req, res, next) => {
+    const { team_robot_id, channel_id, access_hash, offset } = req.query;
+
+    try {
+        const data = await getChannelContacts(team_robot_id, channel_id, access_hash);
+        res.send(data);
+        return next();
+    } catch (err) {
+        console.log(err);
+        return next(new errors.InternalServerError('Internal server error'));
+    }
 });
 
 const port = 1337;
 server.listen(port, () => {
-  console.log('Listening on port %d', port);
+    console.log('Listening on port %d', port);
 });
